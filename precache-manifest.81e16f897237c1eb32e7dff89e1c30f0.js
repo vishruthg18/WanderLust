d4b7dc2a3c4949be6946ecb5dda8ee30c8efded5
Vishruth Guda <vishruthg18@gmail.com>
2022-07-06 17:35:21 -0400

    Updates

---
+++ precache-manifest.81e16f897237c1eb32e7dff89e1c30f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79facda7a979a167e46a3dd4fd39f3a7",
+    "url": "/WanderLust/index.html"
+  },
+  {
+    "revision": "88bf75ac109d777b2775",
+    "url": "/WanderLust/static/css/main.f64c8c26.chunk.css"
+  },
+  {
+    "revision": "4805cb1f96e15a530bbb",
+    "url": "/WanderLust/static/js/2.2bf41a56.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/WanderLust/static/js/2.2bf41a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bf75ac109d777b2775",
+    "url": "/WanderLust/static/js/main.1f054445.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/WanderLust/static/js/main.1f054445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726e2792359ca74e0da4",
+    "url": "/WanderLust/static/js/runtime-main.80c18cf5.js"
+  },
+  {
+    "revision": "bcbb661983ab41776a9187e2a567461c",
+    "url": "/WanderLust/static/media/WanderLustImg.bcbb6619.png"
+  },
+  {
+    "revision": "6aae489b30fa7f4d17f7942ac8ee92db",
+    "url": "/WanderLust/static/media/travelbg.6aae489b.jpg"
+  }
+]);
